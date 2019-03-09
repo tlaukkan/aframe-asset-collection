b969d064a6060c0ea9c06a07ce38284952caaa87
Tommi Laukkanen <tommi.s.e.laukkanen@gmail.com>
2019-03-09 11:16:36 +0200

    Added human avatars from quaternius.
    https://www.patreon.com/quaternius

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# aframe-asset-collection
+# A-Frame asset collection
+
+Assets suitable for using in A-Frame projects.
+
+Individual files and folders have license and attribution information if the files are
+not originally created for this collection.
+
+Special thanks to quaternius: https://www.patreon.com/quaternius
---
+++ avatars/quaternius/human/female/convert.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for filename in *.fbx; do
+    fbx2gltf $filename
+    name="${filename%.*}"
+    gltfname="$name"_out/"$name".gltf
+    perl -pi -w -e 's/HumanArmature\|Female_//g;' $gltfname
+    gltf-import-export $gltfname
+    glbname="$name"_out/"$name".glb
+    mv $glbname .
+    rm -Rf "$name"_out
+done
